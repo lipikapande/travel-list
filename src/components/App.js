052358ab8c03b2c040841c0553c23fc87fcbeb79
lipika <lipikapande@gmail.com>
2025-06-07 23:07:54 +0530

    tip calculator

--- src/components/App.js
+++ src/components/App.js
@@ -48,3 +48,86 @@ export default function App() {
     </div>
   );
 }
+
+// TIP CALCULATOR, SAMPLE CHALLENGE CODE
+
+// import { useState } from "react";
+// import "./styles.css";
+
+// export default function App() {
+//   const [Bill, setBill] = useState(null);
+
+//   return (
+//     <div>
+//       <TipCalculator bill={Bill} onBill={setBill} />
+//     </div>
+//   );
+// }
+
+// function TipCalculator({ bill, onBill }) {
+//   const [per1, setper1] = useState(0);
+//   const [per2, setper2] = useState(0);
+
+//   const tip = (+bill * (per1 + per2)) / 200;
+
+//   function handleReset() {
+//     onBill(null);
+//     setper1(0);
+//     setper2(0);
+//   }
+//   return (
+//     <div>
+//       <BillInput bill={bill} onBill={onBill} />
+//       <SelectPercentage per={per1} onChangePer={setper1}>
+//         How much did you like the service?
+//       </SelectPercentage>
+//       <SelectPercentage per={per2} onChangePer={setper2}>
+//         How much did your friend like the service?
+//       </SelectPercentage>
+//       <Output bill={bill} tip={tip} />
+//       <Reset onReset={handleReset} />
+//     </div>
+//   );
+// }
+
+// function BillInput({ bill, onBill }) {
+//   return (
+//     <div>
+//       <span>How much was the bill?</span>
+//       <input
+//         type="text"
+//         placeholder="Bill value"
+//         value={bill ?? ""}
+//         onChange={(e) => onBill(e.target.value)}
+//       />
+//     </div>
+//   );
+// }
+
+// function SelectPercentage({ children, per, onChangePer }) {
+//   return (
+//     <div>
+//       <span>{children}</span>
+//       <select value={per} onChange={(e) => onChangePer(Number(e.target.value))}>
+//         <option value="0">0%</option>
+//         <option value="5">5%</option>
+//         <option value="10">10%</option>
+//         <option value="20">20%</option>
+//       </select>
+//     </div>
+//   );
+// }
+
+// function Output({ bill, tip }) {
+//   if (!bill) return null;
+
+//   return (
+//     <h3>
+//       You pay {+bill + +tip} ({+bill} + {+tip})
+//     </h3>
+//   );
+// }
+
+// function Reset({ onReset }) {
+//   return <button onClick={onReset}>Reset</button>;
+// }
